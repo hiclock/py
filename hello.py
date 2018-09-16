0a86fd35ec7e45c85a2267a2d6f3bb4fa660e623
hiclock <43112817+hiclock@users.noreply.github.com>
2018-09-16 20:52:15 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,8 +3,14 @@ import tornado.web
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
+        
+class TestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("test Hello, world")
+        
 Handlers=[
     (r"/", MainHandler),
+    (r"/test", TestHandler),
 ]
 application = tornado.web.Application(Handlers)
 if __name__ == "__main__":
